58b343d1a2276c2a43f25884a308d43a52d74b52
D'Andrea Marco <dandreamarco.dm@gmail.com>
2020-04-29 18:57:18 -0300

    ejercicio 3 terminado

--- Unidad02/Capitulo01/LabSintaxis3/LabSintaxis3/Program.cs
+++ Unidad02/Capitulo01/LabSintaxis3/LabSintaxis3/Program.cs
@@ -10,6 +10,18 @@ namespace LabSintaxis3
     {
         static void Main(string[] args)
         {
+      
+            string[] palabras = new String[5];
+           for(int cantIteraciones = 0; cantIteraciones < 5; cantIteraciones++)
+            {
+                System.Console.WriteLine("Ingrese una palabra");
+                palabras[cantIteraciones] = Console.ReadLine();
+            }
+            for(int cantIteraciones = 4; cantIteraciones >= 0; cantIteraciones --)
+            {
+                Console.WriteLine("Palabra posicion  " + cantIteraciones + "\r\n" + palabras[cantIteraciones]);
+            }
+            Console.ReadKey();
         }
     }
 }
