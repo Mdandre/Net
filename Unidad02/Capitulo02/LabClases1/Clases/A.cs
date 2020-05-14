f76ad0389455a928db07cd0d1edf300669d4a5ef
D'Andrea Marco <dandreamarco.dm@gmail.com>
2020-05-14 16:58:56 -0300

    Agregado cap02

---
+++ Unidad02/Capitulo02/LabClases1/Clases/A.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class A
+    {
+        string _NombreInstancia;
+        public string NombreInstancia 
+        {
+          get
+          {
+            return _NombreInstancia;
+          }
+          set
+          {
+            _NombreInstancia = value;
+          }
+        }
+        public A() 
+        {
+            NombreInstancia = "Instancia sin nombre";
+        }
+        public A(string nombre)
+        {
+            NombreInstancia = nombre;
+        }
+        public void MostrarNombre()
+        {
+            Console.WriteLine(NombreInstancia);
+        }
+        public void M1()
+        {
+            Console.WriteLine("El nombre del metodo es M1");
+        }
+        public void M2()
+        {
+            Console.WriteLine("El nombre del metodo es M2");
+        }
+        public void M3()
+        {
+            Console.WriteLine("El nombre del metodo es M3");
+        }
+    }
+    
+}
---
+++ Unidad02/Capitulo02/LabClases1/LabClases1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clases;
+
+namespace LabClases1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A Aa = new A();
+            Aa.M1();
+            Aa.M2();
+            Aa.M3();
+            Aa.MostrarNombre();
+            Console.WriteLine(Aa.NombreInstancia);
+            B Bb = new B();
+            Bb.M4();
+            Bb.M1();
+            Bb.M2();
+            Bb.M3();
+            Bb.MostrarNombre();
+            Console.WriteLine(Bb.NombreInstancia);
+            Console.ReadLine();
+        }
+    }
+}
