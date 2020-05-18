e72565a7b4bcb7751bbee480be02d2067316bfad
D'Andrea Marco <dandreamarco.dm@gmail.com>
2020-05-18 20:53:09 -0300

    Adivina numero

---
+++ Unidad02/Capitulo02/AdivineElJuego/AdivineElJuego/Juego.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdivineElJuego
+{
+    public class Juego
+    {
+        private int _record;
+
+        public Juego()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ComenzarJuego()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void CompararRecord()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Continuar()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void PreguntarMaximo()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void PreguntarNumero()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Unidad02/Capitulo02/AdivineElJuego/AdivineElJuego/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdivineElJuego
+{
+    public class Program
+    {
+        private void Main()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
